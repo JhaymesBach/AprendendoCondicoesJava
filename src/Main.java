3ef3ea89b630f0f89ae76d3f8dd904f934585a12
iurigiroldi <iurigiroldi@gmail.com>
2024-08-21 20:46:36 -0300

    Criação do projeto java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Integer idadePessoa = 20;
+        String nomePessoa = "Jhaymes";
+
+        /*
+         *Operadores Lógicos
+         * > maior
+         */
+
+
+        if (idadePessoa > 21) ;
+        System.out.println("é maior que 21");
+    }else{
+        System.out.println("Nao é maior");
+
     }
+
+
 }
